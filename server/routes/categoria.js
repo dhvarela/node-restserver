7a06f2f96eb80b282c526c756131407335f56daa
Daniel <daniloviciv@hotmail.com>
2019-02-12 23:44:01 +0100

    category model and category CRUD

---
+++ server/routes/categoria.js
@@ -0,0 +1,181 @@
+const express = require('express');
+
+let { tokenVerify, adminRoleVerification } = require('../middlewares/authentication');
+
+let Categoria = require('../models/categoria');
+
+let app = express();
+
+
+// ================
+// show all categories
+// ================
+
+app.get('/categoria', tokenVerify, (req, res) => {
+
+    Categoria.find({})
+        .exec((err, categorias) => {
+
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            res.json({
+                ok: true,
+                categorias
+            })
+        })
+
+});
+
+
+// ================
+// show a category by Id
+// ================
+
+app.get('/categoria/:id', (req, res) => {
+
+    let id = req.params.id;
+
+    Categoria.findById(id, (err, categoriaDB) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!categoriaDB) {
+            return res.status(500).json({
+                ok: false,
+                err: {
+                    message: 'The id is not valid'
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            categoria: categoriaDB
+        })
+
+    })
+
+});
+
+
+// ================
+// create new category
+// ================
+
+app.post('/categoria', tokenVerify, (req, res) => {
+
+    let body = req.body;
+
+    let categoria = new Categoria({
+        descripcion: body.descripcion,
+        usuario: req.usuario._id
+    });
+
+    categoria.save((err, categoriaDB) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!categoriaDB) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            categoria: categoriaDB
+        });
+
+    });
+});
+
+// ================
+// edit category by Id
+// ================
+
+app.put('/categoria/:id', tokenVerify, (req, res) => {
+
+    let id = req.params.id;
+    let body = req.body;
+
+    let descCategoria = {
+        descripcion: body.descripcion
+    };
+
+    Categoria.findByIdAndUpdate(id, descCategoria, { new: true, runValidators: true }, (err, categoriaDB) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!categoriaDB) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            categoria: categoriaDB
+        });
+
+    });
+});
+
+
+// ================
+// delete a category
+// ================
+
+app.delete('/categoria/:id', [tokenVerify, adminRoleVerification], (req, res) => {
+
+    let id = req.params.id;
+
+    Categoria.findByIdAndRemove(id, (err, categoriaDB) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!categoriaDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'The id does not exist'
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            message: 'Categorie deleted'
+        })
+    });
+
+});
+
+
+module.exports = app;
