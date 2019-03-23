0aa29477bf392b533a585c72c689e3664007dbce
Daniel <daniloviciv@hotmail.com>
2019-03-23 22:30:21 +0100

    add upload user image and save in mongodb User entity

--- server/routes/uploads.js
+++ server/routes/uploads.js
@@ -2,16 +2,33 @@ const express = require('express');
 const fileUpload = require('express-fileupload');
 const app = express();
 
+const Usuario = require('../models/usuario');
+
 // default options
 app.use(fileUpload());
 
-app.put('/upload', function(req, res) {
+app.put('/upload/:type/:id', function(req, res) {
+
+    let type = req.params.type;
+    let id = req.params.id;
 
     if (!req.files) {
         return res.status(400).json({
             ok: false,
             err: {
                 message: 'no file selected'
+            }
+        });
+    }
+
+    // check type
+    let validTypes = ['products', 'users'];
+
+    if (validTypes.indexOf(type) < 0) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'Los tipos permitidos son ' + validTypes.join(', ')
             }
         });
     }
@@ -34,7 +51,11 @@ app.put('/upload', function(req, res) {
         });
     }
 
-    sampleFile.mv(`uploads/${ sampleFile.name }`, (err) => {
+    // change file name
+    let fileName = `${ id }-${ new Date().getMilliseconds() }.${ extension }`;
+
+    sampleFile.mv(`uploads/${ type }/${ fileName }`, (err) => {
+
         if (err) {
             return res.status(500).json({
                 ok: false,
@@ -42,11 +63,50 @@ app.put('/upload', function(req, res) {
             });
         }
 
-        res.json({
-            ok: true,
-            message: 'Image uploaded'
-        });
+        userImage(id, res, fileName);
+
     });
 });
+
+function userImage(id, res, fileName) {
+
+    Usuario.findById(id, (err, usuarioDB) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!usuarioDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'User does not exist'
+                }
+            });
+        }
+
+        usuarioDB.img = fileName;
+
+        usuarioDB.save((err, userSaved) => {
+
+            res.json({
+                ok: true,
+                usuario: userSaved,
+                img: fileName
+            })
+
+        });
+
+
+    });
+
+}
+
+function productImage() {
+
+}
 
 module.exports = app;
